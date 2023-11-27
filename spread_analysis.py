8a7bb1fcb284b2e15e31619b39a79433184b9748
Tommy Sullivan <95664771+tsully54@users.noreply.github.com>
2023-11-27 17:56:58 -0600

    Add files via upload

--- spread_analysis.py
+++ spread_analysis.py
@@ -374,8 +374,8 @@ st.dataframe(home_dog_results_sp_df)
 
 ################## COVER PERCENTAGE BY TEAM ##################
 #calculate how teams have done relative to spread
-team_spreads23 = pd.DataFrame(df23['spread_winner'].value_counts(), columns = 'spread_winner')
-team_loss23 = pd.DataFrame(df23['spread_loser'].value_counts(), columns = 'spread_loser')
+team_spreads23 = pd.DataFrame(df23['spread_winner'].value_counts(), columns = ['spread_winner'])
+team_loss23 = pd.DataFrame(df23['spread_loser'].value_counts(), columns = ['spread_loser'])
 st.dataframe(team_spreads23)
 st.write(team_loss23.columns)
 
